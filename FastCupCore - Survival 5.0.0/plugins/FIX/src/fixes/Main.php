3353c76f19e1adfd91af8f6263da31cfb0db66d0
Zloger <79506370+haker20SZs@users.noreply.github.com>
2022-10-23 20:48:17 +0700

    Add files via upload

---
+++ FastCupCore - Survival 5.0.0/plugins/FIX/src/fixes/Main.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Fixes;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase as Base;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\scheduler\CallbackTask;
+use pocketmine\level\Level;
+use pocketmine\entity\Effect;
+
+class Main extends Base implements Listener{
+
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getServer()->getScheduler()->scheduleRepeatingTask(new CallbackTask(array($this, "onEffectFix")), 1);
+	}
+
+	public function onEffectFix(){
+	  foreach($this->getServer()->getOnlinePlayers() as $player){
+		if($player->getLevel()->getName() == "world"){
+	    	$player->addEffect(Effect::getEffect(16)->setDuration(9999*9999*16)->setVisible(false));
+	    }elseif($player->getLevel()->getName() == "ender"){
+	    	$player->removeEffect(16);
+	    }elseif($player->getLevel()->getName() == "nether"){
+	    	$player->removeEffect(16);
+	    }
+	  }
+    }
+
+}
+
+?>
