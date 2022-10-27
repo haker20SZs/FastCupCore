904dd3ecb28ac076222e1e9099162aeb6d7d73d5
Zloger <79506370+haker20SZs@users.noreply.github.com>
2022-10-27 19:42:27 +0700

    Add files via upload

---
+++ FastCupCore - Survival 6.5.0 (beta)/plugins/FIX/src/fixes/Main.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Fixes;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase as Base;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\scheduler\CallbackTask;
+use pocketmine\level\Level;
+use pocketmine\entity\Effect;
+use pocketmine\event\player\PlayerRespawnEvent;
+
+class Main extends Base implements Listener{
+
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getServer()->getScheduler()->scheduleRepeatingTask(new CallbackTask(array($this, "onEffectFix")), 1);
+	}
+
+	public function onRespawn(PlayerRespawnEvent $event){
+	  foreach($this->getServer()->getOnlinePlayers() as $player){
+	  	if($player->getLevel()->getName() == "ender"){
+	  	   $player->teleport(Server::getInstance()->getDefaultLevel()->getSafeSpawn());
+	  	}elseif($player->getLevel()->getName() == "nether"){
+	  	   $player->teleport(Server::getInstance()->getDefaultLevel()->getSafeSpawn());
+	  	}
+	  }
+	}
+
+	public function onEffectFix(){
+	  foreach($this->getServer()->getOnlinePlayers() as $player){
+		if($player->getLevel()->getName() == "world"){
+	    	$player->addEffect(Effect::getEffect(16)->setDuration(9999*9999*16)->setVisible(false));
+	    }elseif($player->getLevel()->getName() == "ender"){
+	    	$player->removeEffect(16);
+	    }elseif($player->getLevel()->getName() == "nether"){
+	    	$player->removeEffect(16);
+	    }
+	  }
+    }
+
+}
+
+?>
